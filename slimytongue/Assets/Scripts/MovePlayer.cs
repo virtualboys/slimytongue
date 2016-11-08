2e0623729fbbe413c4a25e8a8fbca09f048a8a0a
mporte3 <mfporte@gatech.edu>
2016-11-08 17:03:06 -0500

    Revert "minor bug fix for floaty frog."
    This reverts commit ab72a32e5d0ea2cdf9d28e7113a33173af19a0d2.

--- slimytongue/Assets/Scripts/MovePlayer.cs
+++ slimytongue/Assets/Scripts/MovePlayer.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class MovePlayer : MonoBehaviour {
 
@@ -16,15 +17,11 @@ public class MovePlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
 		Vector3 input = Vector3.zero;
 		input.x = Input.GetAxisRaw (playerInput.horizontal);
 		input.z = Input.GetAxisRaw (playerInput.vertical);
-        
-		Vector3 movement = Vector3.zero;
 
-        MaintainRotation();
-        print(transform.rotation);
+		Vector3 movement = Vector3.zero;
 
 		if (m_tongueController.CanMove ()) {
 			movement = Move (input);
@@ -37,44 +34,7 @@ public class MovePlayer : MonoBehaviour {
 		transform.position += movement;
 
 		animator.SetFloat ("Speed", movement.magnitude);
-
-        KeepPlayerGrounded();
 	}
-
-    private void MaintainRotation()
-    {
-        var rot = transform.rotation;
-        rot.Set(rot.x, 0.0f, rot.z, 1.0f);
-        transform.rotation = rot;
-    }
-
-    private void KeepPlayerGrounded()
-    {
-        // start ray at player pos
-        Ray ray = new Ray(transform.position, Vector3.down);
-
-        RaycastHit hitInfo;
-
-        // get da damn layer for our ground. should have a mesh collider
-        LayerMask layer = 1 << LayerMask.NameToLayer("Ground");
-
-        //cast ray
-        if (Physics.Raycast(ray, out hitInfo, layer))
-        {
-            //get where our raycast hit on the ground
-            float z = hitInfo.point.z;
-
-            // get the current position
-            Vector3 pos = transform.position;
-            
-            // change the z of our current position so that boy don't leave the ground.
-            pos.z = z;
-
-            // override the current position with the grounded position
-            transform.position = pos;
-        }
-        //todo: might need an else here in case he somehow gets flipped upside down???
-    }
 
 	private void Aim(Vector3 input) {
 		if (input == Vector3.zero) {
